4a6dfda17950c8f1d1560b6aa15ba7b680bcb77d
josemarb96 <josemarb96@gmail.com>
2016-11-14 13:13:10 +0100

    DataBinding_VM_CS

---
+++ DataBinding_VM_CS/Models/clsListado.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DataBinding_VM_CS.Models;
+using System.Collections.ObjectModel;
+
+namespace DataBinding_VM_CS.Models
+{
+    public class clsListado
+    {
+        public ObservableCollection<clsPersona> getListadoPersonas()
+        {
+            ObservableCollection<clsPersona> lista = new ObservableCollection<clsPersona>();
+            
+            lista.Add(new clsPersona(1,"David", "Benítez Rasero", new DateTime(2012,12,12), "Montequinto", "123456789"));
+            lista.Add(new clsPersona(2,"Makoto", "Naegi", new DateTime(1996, 12, 8), "Japon Street", "123789456"));
+            lista.Add(new clsPersona(3,"Sayaka", "Maizono", new DateTime(1999, 4, 25), "Quinto Pino, Pinar 12", "147258369"));
+            lista.Add(new clsPersona(4,"Kyouko", "Kirigiri", new DateTime(2015, 7, 7), "Av. de la Constitucion Nº12", "963852147"));
+            lista.Add(new clsPersona(1, "Maria", "Benítez Brasero", new DateTime(2012, 12, 12), "Montequinto", "123456789"));
+            lista.Add(new clsPersona(2, "Marakoto", "Nagini", new DateTime(1996, 12, 8), "Japon Street", "123789456"));
+            lista.Add(new clsPersona(3, "Sasayaka", "Movistar", new DateTime(1999, 4, 25), "Quinto Pino, Pinar 12", "147258369"));
+            lista.Add(new clsPersona(4, "Kyokyouko", "Kirikú", new DateTime(2015, 7, 7), "Av. de la Constitucion Nº12", "963852147"));
+            lista.Add(new clsPersona(1, "Antonio", "Marquez Rasero", new DateTime(2012, 12, 12), "Montequinto", "123456789"));
+            lista.Add(new clsPersona(2, "Melendi", "AleAle", new DateTime(1996, 12, 8), "Japon Street", "123789456"));
+            lista.Add(new clsPersona(3, "Soraya", "OnoWifi", new DateTime(1999, 4, 25), "Quinto Pino, Pinar 12", "147258369"));
+            lista.Add(new clsPersona(4, "Manolo", "Perez", new DateTime(2015, 7, 7), "Av. de la Constitucion Nº12", "963852147"));
+
+            return lista;
+        }
+    }
+}
---
+++ DataBinding_VM_CS/ViewModels/clsMainPageVM.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using DataBinding_VM_CS.Models;
+using System.ComponentModel;
+
+namespace DataBinding_VM_CS.ViewModels
+{
+    public class clsMainPageVM: INotifyPropertyChanged
+    {
+        private clsPersona _personaSeleccionada;
+        private ObservableCollection<clsPersona> _listado;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public clsPersona personaSeleccionada
+        {
+            get
+            {
+                return _personaSeleccionada;
+            }
+            set
+            {
+                _personaSeleccionada = value;
+                OnPropertyChanged("personaSeleccionada"); //Poner el mismo nombre que en el public
+            }
+        }
+        public ObservableCollection<clsPersona> listado
+        {
+            get
+            {
+                return _listado;
+            }            
+        }
+        public clsMainPageVM()
+        {
+            clsListado lista = new clsListado();
+            _listado = lista.getListadoPersonas();
+        }
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+    }
+}
